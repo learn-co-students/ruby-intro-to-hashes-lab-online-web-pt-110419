75826945f16e0e00e8ed7a21d10dfc2c97f9b478
Mintra Phophaijit <bluembc@gmail.com>
2019-12-10 20:35:36 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,27 +1,54 @@
 def new_hash
-
+new_hash={}
 end
 
 def actor
-
+actor = { name: "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {},
+	:railroads => {:pieces => 4 }
+	}
+
 
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	monopoly = {},
+	:railroads => {:pieces => 4
+	  
+	
+	}
 
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	monopoly = {},
+	:railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25,
+	:two_piece_owned => 50, :third_piece_owned => 100}
+	
+	}
+	:names=>
 
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+def monopoly_with_third_tier
+	monopoly = {},
+	:railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25,
+	:two_piece_owned => 50, :third_piece_owned => 100}
+	
+	}
+	:names=> {:reading_railroad => {:mortgage_value => "$100"
+	}
+	:pennsylvania_railroad => {:mortgage_value => "$200"
+	}
+	:b_and_o_railroad => { :mortgage_value => "$400"
+	}
+	:shortline => {:mortgage_value => "$800"
+	}
+
+end
 
 end
